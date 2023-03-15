f59f56c729f096cce196237aee5c613d06ce9541
sdange <sdange@paypal.com>
2023-03-15 19:43:39 +0530

    react-tours

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Loading from './components/Loading'
+import Tours from './components/Tours'
+
+const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const currentTours = await response.json()
+      console.log(currentTours)
+      setTours(currentTours)
+      setIsLoading(false)
+    } catch (error) {
+      setIsLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
+}
+
+export default App
